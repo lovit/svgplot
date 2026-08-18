2c639cfcea3a32566cd33eb2d24745cf8e965df7
lovit <soy.lovit@gmail.com>
2026-08-19 03:25:24 +0900

    feat(charts): regplot을 공개 API에 등록

--- src/svgplot/charts/__init__.py
+++ src/svgplot/charts/__init__.py
@@ -9,6 +9,7 @@ from svgplot.charts.ecdf import ecdfplot
 from svgplot.charts.histogram import histplot
 from svgplot.charts.line import lineplot
 from svgplot.charts.pie import pieplot
+from svgplot.charts.regression import regplot
 from svgplot.charts.scatter import scatterplot
 from svgplot.charts.sparkline import sparkline
 from svgplot.charts.treemap import treemap
@@ -21,6 +22,7 @@ __all__ = [
     "histplot",
     "lineplot",
     "pieplot",
+    "regplot",
     "scatterplot",
     "sparkline",
     "treemap",
