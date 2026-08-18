3b58fe0baa8797624215efb4b6bf4ad59dd26988
lovit <soy.lovit@gmail.com>
2026-08-19 01:54:17 +0900

    feat(stats): 회귀 API를 stats 공개 API에 등록

--- src/svgplot/stats/__init__.py
+++ src/svgplot/stats/__init__.py
@@ -1,4 +1,5 @@
-"""Statistical transforms: interpolation, histogram binning, quantiles, box-plot statistics."""
+"""Statistical transforms: interpolation, histogram binning, quantiles, box-plot statistics,
+linear regression."""
 
 from __future__ import annotations
 
@@ -6,13 +7,18 @@ from svgplot.stats.binning import histogram_bins
 from svgplot.stats.box import BoxStats, box_stats
 from svgplot.stats.interpolate import InterpolatedCurve, interpolate
 from svgplot.stats.quantile import quantile, quantiles
+from svgplot.stats.regression import LinearFit, RegressionBand, confidence_band, linear_fit
 
 __all__ = [
     "BoxStats",
     "InterpolatedCurve",
+    "LinearFit",
+    "RegressionBand",
     "box_stats",
+    "confidence_band",
     "histogram_bins",
     "interpolate",
+    "linear_fit",
     "quantile",
     "quantiles",
 ]
